2436048e89af43453af66f8bda40621522f2d484
vignesh <r.vignesh88@gmail.com>
2022-11-07 17:32:14 +0530

    fix8

--- modules/helm/kube-prometheus-stack/thanos-querier-service.tf
+++ modules/helm/kube-prometheus-stack/thanos-querier-service.tf
@@ -4,12 +4,6 @@ data "kubectl_file_documents" "thanosquerierservice" {
   content = file("${path.module}/thanos-querier-service.yaml")
 }
 
-resource "kubernetes_manifest" "thanosquerierservice" {
-  for_each = toset(data.kubectl_file_documents.thanosquerierservice.documents)
-  manifest = yamldecode(each.value)
-  depends_on = [
-    kubernetes_secret.generic,
-    helm_release.prometheus,
-    kubernetes_manifest.thanosquerierdeployment
-  ]
+resource "kubectl_manifest" "thanosquerierservice" {
+    yaml_body = yamldecode(data.kubectl_file_documents.thanosquerierservice.documents[0])
 }
