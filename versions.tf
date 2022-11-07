077768ed012c66e42d53adc8277576f7e6a349c0
vignesh <r.vignesh88@gmail.com>
2022-11-07 15:37:25 +0530

    fix2

--- versions.tf
+++ versions.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "2.1.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.11.2"
+    }
   }
 
   required_version = "~> 1.2.0"
