48f87f0590554a8a11e638a3efbe72c20da49704
vignesh <r.vignesh88@gmail.com>
2022-11-07 15:41:22 +0530

    fix3

--- modules/helm/kube-prometheus-stack/providers.tf
+++ modules/helm/kube-prometheus-stack/providers.tf
@@ -1,3 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.1.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.11.2"
+    }
+  }
+}
 /*data "terraform_remote_state" "eks" {
   backend = "local"
   config = {
--- versions.tf
+++ versions.tf
@@ -9,17 +9,6 @@ terraform {
       source  = "hashicorp/random"
       version = "3.1.0"
     }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.1.0"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "1.11.2"
-    }
   }
-
   required_version = "~> 1.2.0"
 }
-
