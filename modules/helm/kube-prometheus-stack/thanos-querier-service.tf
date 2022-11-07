6897baa1c6d2b1905422fd689d3d84b155b087e4
vignesh <r.vignesh88@gmail.com>
2022-11-07 17:51:37 +0530

    fix11

--- modules/helm/kube-prometheus-stack/thanos-querier-service.tf
+++ modules/helm/kube-prometheus-stack/thanos-querier-service.tf
@@ -8,13 +8,18 @@ resource "kubectl_manifest" "thanosquerierservice" {
     yaml_body = <<YAML
     apiVersion: v1
     kind: Service
+    metadata:
+      name: thanos-querier
+      namespace: monitoring
+      labels:
+        app: thanos-querier
     spec:
-        type: ClusterIP
-        ports:
-        - name: http
-            port: 9090
-            targetPort: http
-        selector:
-            app: thanos-querier
+      type: ClusterIP
+      ports:
+      - name: http
+        port: 9090
+        targetPort: http
+      selector:
+        app: thanos-querier
     YAML
 }
