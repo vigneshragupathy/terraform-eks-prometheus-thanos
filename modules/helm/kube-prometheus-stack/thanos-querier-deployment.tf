13c06451b9110f4cb36153288eec04191d02e9d8
vignesh <r.vignesh88@gmail.com>
2022-11-07 17:30:49 +0530

    fix7

--- modules/helm/kube-prometheus-stack/thanos-querier-deployment.tf
+++ modules/helm/kube-prometheus-stack/thanos-querier-deployment.tf
@@ -4,6 +4,6 @@ data "kubectl_file_documents" "thanosquerierdeployment" {
   content = file("${path.module}/thanos-querier-deployment.yaml")
 }
 
-resource "kubernetes_manifest" "thanosquerierdeployment" {
+resource "kubectl_manifest" "thanosquerierdeployment" {
   yaml_body = yamldecode(data.kubectl_file_documents.thanosquerierdeployment.documents[0])
 }
